b0325eff5d4799d50f9a4ae29b69016428c2b1d3
Edith Mandujano <edithmandujano001@gmail.com>
2017-04-18 11:25:30 -0500

    Ejercicio | Panda Texts

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 var pandaUno=document.getElementById("pandaUno");
-var cerrarBtns=document.getElementByTagName("cerrar");
-console.log(hola);
-
-funcion cerraUno(){
+var cerrarBtns=document.getElementsByTagName("cerrar");
+console.log(cerrarBtns);
+function cerraUno(){
   pandaUno.style.display=none;
 }
